e887c02fd844e8012f376d700d5eaeaa77be56ab
jutytu <97400439+jutytu@users.noreply.github.com>
2024-09-07 20:19:24 +0200

    pdf function correction

--- files/pdf.py
+++ files/pdf.py
@@ -1,190 +1,108 @@
+# Calculating the PDF for correlation of cosmic rays and earthquakes intensity changes over a 
+# period of 1675 days (335 intervals of 5 days).
+
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-import glob
-import statistics
-import os
 import math
-from time import mktime
 from datetime import datetime, date, timedelta, timezone
-import sys
+from scipy.stats import binom
 
 
-# d = '2014-03-02 22:07:12'
-# l = 335
-# t = '5D'
-# shift = 15
-
-# usecols = ["time", "latitude", "longitude", "mag"]
-# files = os.path.join(r'C:\Users\Ja\OneDrive\Dokumenty\Materiały\credo\eq', "*.csv")
-# files = glob.glob(files)
-# df = pd.concat(map(pd.read_csv, files), ignore_index=True)
-# df = df[usecols]
-# df = df[(df['mag'] >= 4)]  # save only row where mag >= 4
-# # df.to_csv('C:/Users/Ja/Downloads/datatime/Data_analysis/eq_data.csv', index=False)
-# # create second file with sum (from every day) and mean from N days (5 days)
-# df["time"] = df.apply(
-#     lambda x: pd.to_datetime(x['time'].split(".")[0].replace("T", " "), format='%Y-%m-%d %H:%M:%S'), axis=1)
-
-def pdf(d, l, t, shift):
-    d = pd.to_datetime(d) + timedelta(days=shift)
-    d_end = d + timedelta(days=(l - 1) * int(t[:-1]))
-
-    if d < pd.to_datetime('2005-03-30 20:52:00'):
-        print('too early')
-        sys.exit(1)
-
-    df = pd.read_csv(r'C:\Users\Ja\Downloads\datatime\Data_analysis\eq_data.csv', sep=",")
-    df['time'] = pd.to_datetime(df['time'])
-    df = df[(df['time'] >= d)]
-    df.index = range(len(df))
-    new_row = pd.DataFrame({'time': pd.to_datetime(d), 'latitude': 0, 'longitude': 0, 'mag': 0},
+def pdf(d, l, t, shift,shift2):
+    d = pd.to_datetime(d) +timedelta(days=shift2)
+    d_end = d + timedelta(days=l * int(t[:-1]))
+    print("datetime dla EQ: ",d,d_end)
+    eq_df = pd.read_csv(r'C:\Users\Ja\Downloads\datatime\Data_analysis\eq_data.csv', sep=",")
+    eq_df['time'] = pd.to_datetime(eq_df['time'])
+    eq_df = eq_df[(eq_df['time'] >= d - timedelta(days=shift))]
+    eq_df.index = range(len(eq_df))
+    # setting indexes
+    new_row = pd.DataFrame({'time': pd.to_datetime(d) - timedelta(days=shift), 'latitude': 0, 'longitude': 0, 'mag': 0},
                            index=[0])
-    # simply concatenate both dataframes
-    df = pd.concat([new_row, df]).reset_index(drop=True)
+    eq_df = pd.concat([new_row, eq_df]).reset_index(drop=True)
 
-    df = df[(df['time'] <= pd.to_datetime(d_end))]
-    df.index = range(len(df))
-    new_row = pd.DataFrame({'time': pd.to_datetime(d_end), 'latitude': 0, 'longitude': 0, 'mag': 0},
-                           index=[len(df)])
-    df = pd.concat([new_row, df]).reset_index(drop=True)
+    eq_df = eq_df[(eq_df['time'] <= pd.to_datetime(d_end))]
+    eq_df['date'] = eq_df['time']
+    eq_df = eq_df[["date", 'mag']]
 
-    # df.to_csv('C:/Users/Ja/Downloads/datatime/Data_analysis/eq_data.csv', index=False)
-    df['date'] = df['time']
-    df2 = df[["time", "date", 'mag']].copy()
+    # eq_df = eq_df.groupby(pd.Grouper(key='date', freq=t, origin='start'), as_index=False)['mag'].agg(['mean', 'sum', "count"])
+    eq_df = eq_df.groupby(pd.Grouper(key='date', freq=t, origin='start'), as_index=False)['mag'].agg(["count", "sum"])
+    eq_df.at[0, 'count'] = eq_df.at[0, 'count'] - 1
 
-    df2 = df2.groupby(pd.Grouper(key='date', freq=t, origin='start'), as_index=False)['mag'].agg(['sum', "count"])
-    # df2 = df2.groupby(pd.Grouper(key='date', freq='T'), as_index=False)['sum'].agg(['mean'])
-    # df2["mean"] = df2.apply(lambda x: round(x['mean'], 1), axis=1)
-    df2.at[0, 'count'] = df2.at[0, 'count'] - 1
-    df2.at[len(df2) - 1, 'count'] = df2.at[len(df2) - 1, 'count'] - 1
-    df2['date'] = df2['date'].dt.strftime("%Y-%m-%d %H:%M:%S")
-    df2['median'] = 0
-    df2.to_csv('C:/Users/Ja/Downloads/datatime/Data_analysis/eq_data_msum.csv', index=False)
-
-    eq_df = df2
-    #
-    # list = []
-    # n = 0
-    #
-    # while n < eq_df.size / 4:
-    #     list.append(eq_df.at[n, 'sum'])
-    #     n = n + 1
-
-    eq_df['median'] = eq_df['sum'].median()
-
-    eq_df['A'] = eq_df['sum'] / eq_df['median'] - 1
-
-    eq_df.to_csv('C:/Users/Ja/Downloads/datatime/Data_analysis/eq_data_A.csv', index=False)
-
-    # print(eq_df.head(400).to_string())
-
-    d = d - timedelta(days=shift)
-    d_end = d + timedelta(days=(l - 1) * int(t[:-1]))
+    d = d - timedelta(days=shift2) # przesuniecie o 10
+    print("datetime dla Auger: ",d,d_end)
+    d_end = d + timedelta(days=l * int(shift))
+    print(d_end)
 
     pa_df = pd.read_csv(r'C:\Users\Ja\Downloads\datatime\Data_analysis\pierre_auger\scalers.csv', sep=",")
-
-    # pa_df['date'] = datetime.fromtimestamp(pa_df['time'])
     pa_df['date'] = pd.to_datetime(pa_df['time'], unit='s')
-    pa_df['date'] = pa_df['date'].dt.strftime("%Y-%m-%d %H:%M:%S")
-    pa_df['date'] = pd.to_datetime(pa_df['date'])
-    # print(pa_df.head(50).to_string())
-
-    pa_df = pa_df[(pa_df['date'] >= (pd.to_datetime(d)) - timedelta(days=int(t[:-1])))]
+    pa_df = pa_df[(pa_df['date'] > (pd.to_datetime(d)) - timedelta(days=shift))]
     pa_df.index = range(len(pa_df))
     new_row = pd.DataFrame({'time': 0, 'rateCorr': 0, 'arrayFraction': 0, 'rateUncorr': 0, 'pressure': 0,
-                            'date': pd.to_datetime(d) - timedelta(days=int(t[:-1]))}, index=[0])
+                            'date': pd.to_datetime(d) - timedelta(days=shift)}, index=[0])
     pa_df = pd.concat([new_row, pa_df]).reset_index(drop=True)
-
     pa_df = pa_df[(pa_df['date'] <= pd.to_datetime(d_end))]
-    pa_df.index = range(len(pa_df))
-    new_row = pd.DataFrame({'time': 0, 'rateCorr': 0, 'arrayFraction': 0, 'rateUncorr': 0, 'pressure': 0,
-                            'date': pd.to_datetime(d_end)}, index=[len(pa_df)])
-    pa_df = pd.concat([new_row, pa_df]).reset_index(drop=True)
-
     pa_df = pa_df.groupby(pd.Grouper(key='date', freq=t, origin='start'), as_index=False)['rateCorr'].agg(
-        ['sum', "count"])
-
-    # print(pa_df.head(400).to_string())
-
-    pa_df['avg'] = 0
+        ['mean', 'sum', "count"])
     pa_df.at[0, 'count'] = pa_df.at[0, 'count'] - 1
-    pa_df.at[len(pa_df) - 1, 'count'] = pa_df.at[len(pa_df) - 1, 'count'] - 1
-
-    # print(pa_df.head(500).to_string())
-    pa_df.index = range(len(pa_df))
-
-    n = 0
-    while n < len(pa_df):
-        if pa_df.at[n, 'count'] != 0:
-            pa_df.at[n, 'avg'] = pa_df.at[n, 'sum'] / pa_df.at[n, 'count']
-        n = n + 1
-
-    pa_df['delta'] = 0
-    # print(pa_df.head(350).to_string())
-
-    n = 1
-    while n < len(pa_df):
-        pa_df.at[n, 'delta'] = abs(pa_df.at[n, 'avg'] - pa_df.at[n - 1, 'avg'])
-        n = n + 1
-
-    pa_df = pa_df.drop(pa_df.index[0])
-    pa_df.index = range(len(pa_df))
-
-    # list = []
-    # n = 0
-    #
-    # while n < len(pa_df):
-    #     list.append(pa_df.at[n, 'delta'])
-    #     n = n + 1
-
-    pa_df['median'] = pa_df['delta'].median()
-
-    pa_df['B'] = pa_df['delta'] / pa_df['median'] - 1
-
-    pa_df.index = range(len(pa_df))
-
-    # print(pa_df.head(50).to_string())
-    # print(eq_df.head(50).to_string())
 
     c = pd.DataFrame()
-    c['date eq'] = eq_df['date']
-    c['A'] = eq_df['A']
-    c['date pa'] = pa_df['date']
-    c['B'] = pa_df['B']
-    c['c'] = np.sign(c['A'] * c['B'])
-    c = c.head(l)
-    # or pa_df.at[n-1, 'count'] == 0 or c.at[n, 'c'] == 0 or eq_df.at[n, 'sum'] == 0
-    n = 1
-    to_drop = []
-    while n < len(c.index):
-        if (pa_df.at[n, 'count'] == 0 or pa_df.at[n - 1, 'count'] == 0 or c.at[n, 'c'] == 0 or eq_df.at[n, 'sum'] == 0):
-            to_drop.append(n)
-        n = n + 1
+    c['pa date'] = pa_df['date']
+    c['eq date'] = eq_df['date']
+    c['pa sum'] = pa_df['sum']
+    c['pa count'] = pa_df['count']
+    c['eq sum'] = eq_df['sum']
+    c["pa mean"] = pa_df['mean']
 
-    c = c.drop(c.index[to_drop])
+    c['pa delta3'] = abs(c['pa mean'].diff())
+
     c.index = range(len(c))
-    # print(c.head(450).to_string())
+    c['pa median'] = c['pa delta3'].median()  # calculating the median before removing empty elements
+
+    c = c[(c['pa delta3'] > 0)]  # delete intervals with no data days 
+
+    c['eq median'] = c['eq sum'].median()
+    c['eq diff'] = c.apply(lambda x: round(x["eq sum"] - x['eq median'], 4), axis=1)
+
+    c['A'] = c['eq sum']/c['eq median'] - 1
+    c['B'] = c['pa delta3']/c['pa median'] - 1
+    c['c'] = np.sign(c['A'] * c['B'])
+
+    # c = c.drop(c.index[0])
+    c.index = range(len(c))
+
+    # print(eq_df.head())
+    # print(pa_df.head())
+    # print(c.head().to_string())
+    # print(c.tail().to_string())
+    #
+    # print(len(c))
+    # print(c['c'].value_counts()[1])
+    coulumn = ["pa date", "eq date", "pa mean", "eq sum", "pa delta3", "eq median", "pa median", "eq diff", "A", "B", "c"]
+    c.to_csv("pdf3.csv", index=False, columns=coulumn)
+
+    tmp_c = c[(c['c'] > 0)] # nowe
+    # newton = 1-binom.cdf(len(tmp_c),len(c),0.5,1) # nowe
+    CDF = 1-binom.cdf(len(tmp_c),len(c),0.5,1) # nowe
+    print("CDF: ", CDF)
 
     n = c['c'].value_counts()[1]
     N = len(c)
-    newton = math.comb(N, n)*((1/2)**N)
-    return newton
+    PDF = math.comb(N, n) * ((1 / 2) ** N)
+    return PDF
 
 
+# 2014-04-02 22:07:12
+print("PDF: ", pdf('2014-04-02 22:07:12', 335, '5D', 5,15))
 
 data = pd.DataFrame()
 date = pd.to_datetime('2012-03-02 22:07:12')
-# print(pdf(date.strftime("%Y-%m-%d %H:%M:%S"), 335, '5D', 15))
-# print(pdf('2014-04-02 22:07:12', 335, '5D', 15))
 
 
-while date <= pd.to_datetime('2014-06-02 22:07:12'):
-    row = pd.DataFrame({'date': date, 'pdf': pdf(date.strftime("%Y-%m-%d %H:%M:%S"), 335, '5D', 15)},
-                       index = [len(data)])
-    data = pd.concat([row, data]).reset_index(drop=True)
-    data.to_csv('C:/Users/Ja/Downloads/datatime/Data_analysis/pdf_data2.csv', sep= ";", index=False)
-    date = date + timedelta(hours = 6)
+while date <= pd.to_datetime('2014-06-21 22:07:12'):
     print(date)
-
-
+    row = pd.DataFrame({'date': date, 'pdf': pdf(date.strftime("%Y-%m-%d %H:%M:%S"), 335, '5D', 5, 15)},
+                       index = [0])
+    data = pd.concat([row, data]).reset_index(drop=True)
+    data.to_csv('C:/Users/Ja/Downloads/datatime/Data_analysis/pdf_data4.csv', index=False)
+    date = date + timedelta(days = 1)
